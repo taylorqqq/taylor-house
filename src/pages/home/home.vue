0e255a6478ebc35d3947a1d243988fe86fab2f22
王鑫 <18827029354@163.com>
2022-05-29 11:38:52 +0800

    修改细节描述

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -2,7 +2,7 @@
   <div class="home-container">
     <div class="remark-section">
       <div class="remark-content">
-        <p class="remark-title">使用说明</p>
+        <p class="remark-title">项目说明</p>
         <p class="reamrk-desc">1、本项目分为1个主项目、2个微前端项目和1个iframe项目，共4个项目。</p>
         <p class="reamrk-desc">2、本项目前端用来实践和学习Vue3、Vite、qiankun等技术栈。</p>
         <p class="reamrk-desc">3、本项目的CI/CD采用Jenkins结合Gitee。</p>
