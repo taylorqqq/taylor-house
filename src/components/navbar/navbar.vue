5150c70aa5dfc37b936c9cf8b64772363dc1babc
王鑫 <18827029354@163.com>
2022-06-13 12:22:10 +0800

    规范文件格式

--- src/components/navbar/navbar.vue
+++ src/components/navbar/navbar.vue
@@ -6,13 +6,10 @@
           <transition-group name="breadcrumb">
             <!-- 防止面包屑导航出现 首页/首页， v-if="route.name!='home'" -->
             <el-breadcrumb-item
-              v-for="(route, i) in state.crumbList"
-              :key="route.name"
-              :to="{ name: route.name }"
-              :class="{
+              v-for="(route, i) in state.crumbList" :key="route.name"
+              :to="{ name: route.name }" :class="{
                 'is-last-link': i == state.crumbList.length - 1,
-              }"
-            >
+              }">
               <span>{{ $t(route.meta.name) }}</span>
             </el-breadcrumb-item>
           </transition-group>
@@ -59,158 +56,158 @@ import { useRouter } from "vue-router";
 import screenfull from "screenfull";
 
 export default defineComponent({
-  setup() {
-    const router = useRouter();
-    const store = useStore();
-    let state: any = reactive({
-      crumbList: computed(() => store.getters.getCrumbList),
-    });
-    let fullScreenHandler = () => {
-      screenfull.isEnabled && screenfull.toggle();
-    };
-    const handleCommand = () => {
-      window.localStorage.clear();
-      store.dispatch("CLEAR_ALL_ROUTES");
-      setTimeout(() => {
-        router.push("/login");
-      }, 500);
-    };
-    return {
-      state,
-      handleCommand,
-      fullScreenHandler,
-    };
-  },
+    setup() {
+        const router = useRouter();
+        const store = useStore();
+        let state: any = reactive({
+            crumbList: computed(() => store.getters.getCrumbList),
+        });
+        let fullScreenHandler = () => {
+            screenfull.isEnabled && screenfull.toggle();
+        };
+        const handleCommand = () => {
+            window.localStorage.clear();
+            store.dispatch("CLEAR_ALL_ROUTES");
+            setTimeout(() => {
+                router.push("/login");
+            }, 500);
+        };
+        return {
+            state,
+            handleCommand,
+            fullScreenHandler,
+        };
+    },
 });
 </script>
 
 <style lang="scss">
 .nav-container {
-  height: 100%;
-  margin-left: 200px;
-  width: calc(100% - 200px);
-  z-index: 9999;
+    height: 100%;
+    margin-left: 200px;
+    width: calc(100% - 200px);
+    z-index: 9999;
 
-  .app-content {
-    position: fixed;
-    top: 50px;
-    left: 200px;
-    right: 0;
-    bottom: 0;
-    overflow: scroll;
-  }
-
-  .nav-top {
-    border-bottom: 1px solid #e5e5e5;
-    height: 50px;
-    line-height: 50px;
-    position: fixed;
-    left: 200px;
-    top: 0;
-    right: 0;
-    background: #fff;
-    z-index: 1000;
-    transition: left 0.25s;
-    display: flex;
-    justify-content: space-between;
-
-    .tool-section {
-      width: 20%;
-      display: flex;
-
-      .btn-section {
-        width: 50%;
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-
-        .btn {
-          display: flex;
-          justify-content: flex-end;
-          align-items: center;
-          cursor: pointer;
-          margin-right: 20px;
-
-          .btn-title {
-            margin-left: 10px;
-            font-size: 15px;
-            color: #1d99e3;
-          }
-        }
-
-        .line {
-          width: 1px;
-          height: 20px;
-          background-color: #a4a4a4;
-        }
-      }
-
-      .user-section {
-        width: 50%;
-        padding-right: 20px;
-        text-align: right;
-
-        .el-dropdown {
-          height: 50px;
-          cursor: pointer;
-
-          .el-tooltip__trigger {
-            display: flex;
-            justify-content: flex-end;
-            align-items: center;
-          }
-
-          .header-img {
-            width: 30px;
-            height: 30px;
-            border-radius: 50%;
-          }
-
-          .username {
-            font-size: 15px;
-            margin-left: 10px;
-            margin-right: 10px;
-            // color: #1d99e3;
-          }
-        }
-      }
+    .app-content {
+        position: fixed;
+        top: 50px;
+        left: 200px;
+        right: 0;
+        bottom: 0;
+        overflow: scroll;
     }
 
-    .breadcrumb-section {
-      width: 15%;
-      padding-left: 20px;
-      text-align: left;
-
-      .el-breadcrumb {
+    .nav-top {
+        border-bottom: 1px solid #e5e5e5;
         height: 50px;
         line-height: 50px;
+        position: fixed;
+        left: 200px;
+        top: 0;
+        right: 0;
+        background: #fff;
+        z-index: 1000;
+        transition: left 0.25s;
+        display: flex;
+        justify-content: space-between;
 
-        // .el-breadcrumb__item {
-        //   .el-breadcrumb__separator {
-        //     visibility: hidden;
+        .tool-section {
+            width: 20%;
+            display: flex;
 
-        //     &:first-child {
-        //       visibility: hidden;
-        //     }
-        //   }
-        // }
-      }
+            .btn-section {
+                width: 50%;
+                display: flex;
+                justify-content: flex-end;
+                align-items: center;
 
-      .breadcrumb-enter,
-      .breadcrumb-leave-active {
-        opacity: 0;
-        transform: translateX(20px);
-      }
+                .btn {
+                    display: flex;
+                    justify-content: flex-end;
+                    align-items: center;
+                    cursor: pointer;
+                    margin-right: 20px;
 
-      .breadcrumb-enter-active,
-      .breadcrumb-leave-active {
-        transition: all 0.6s;
-      }
+                    .btn-title {
+                        margin-left: 10px;
+                        font-size: 15px;
+                        color: #1d99e3;
+                    }
+                }
 
-      .breadcrumb-leave-active {
-        position: absolute;
-      }
+                .line {
+                    width: 1px;
+                    height: 20px;
+                    background-color: #a4a4a4;
+                }
+            }
+
+            .user-section {
+                width: 50%;
+                padding-right: 20px;
+                text-align: right;
+
+                .el-dropdown {
+                    height: 50px;
+                    cursor: pointer;
+
+                    .el-tooltip__trigger {
+                        display: flex;
+                        justify-content: flex-end;
+                        align-items: center;
+                    }
+
+                    .header-img {
+                        width: 30px;
+                        height: 30px;
+                        border-radius: 50%;
+                    }
+
+                    .username {
+                        font-size: 15px;
+                        margin-left: 10px;
+                        margin-right: 10px;
+                        // color: #1d99e3;
+                    }
+                }
+            }
+        }
+
+        .breadcrumb-section {
+            width: 15%;
+            padding-left: 20px;
+            text-align: left;
+
+            .el-breadcrumb {
+                height: 50px;
+                line-height: 50px;
+
+                // .el-breadcrumb__item {
+                //   .el-breadcrumb__separator {
+                //     visibility: hidden;
+
+                //     &:first-child {
+                //       visibility: hidden;
+                //     }
+                //   }
+                // }
+            }
+
+            .breadcrumb-enter,
+            .breadcrumb-leave-active {
+                opacity: 0;
+                transform: translateX(20px);
+            }
+
+            .breadcrumb-enter-active,
+            .breadcrumb-leave-active {
+                transition: all 0.6s;
+            }
+
+            .breadcrumb-leave-active {
+                position: absolute;
+            }
+        }
     }
-  }
 }
 </style>
